f9349a178a90b30d6e06e890c392be0e848f1857
jkagnes <v-maprem@microsoft.com>
2016-03-04 12:28:56 -0800

    Initial commit
    Initial version

---
+++ LessonOne/app/src/main/java/com/example/tomabot/lessonone/LessonOneRenderer.java
@@ -0,0 +1,435 @@
+package com.example.tomabot.lessonone;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.SystemClock;
+import android.util.Log;
+
+
+/**
+ * Created by tomabot on 3/4/15.
+ */
+
+public class LessonOneRenderer implements GLSurfaceView.Renderer {
+
+    private static final String LESSONONE_RENDERER = "LessonOneRenderer:";
+
+    /**
+     * Store the model matrix. This matrix is used to move models from object space (where each model can be thought
+     * of being located at the center of the universe) to world space.
+     */
+    private float[] mModelMatrix = new float[16];
+
+    /**
+     * Store the view matrix. This can be thought of as our camera. This matrix transforms world space to eye space;
+     * it positions things relative to our eye.
+     */
+    private float[] mViewMatrix = new float[16];
+
+    /** Store the projection matrix. This is used to project the scene onto a 2D viewport. */
+    private float[] mProjectionMatrix = new float[16];
+
+    /** Allocate storage for the final combined matrix. This will be passed into the shader program. */
+    private float[] mMVPMatrix = new float[16];
+
+    /** Store our model data in a float buffer. */
+    private final FloatBuffer mTriangle1Vertices;
+    private final FloatBuffer mTriangle2Vertices;
+    private final FloatBuffer mTriangle3Vertices;
+    private final FloatBuffer mSquareVertices;
+
+    /** This will be used to pass in the transformation matrix. */
+    private int mMVPMatrixHandle;
+
+    /** This will be used to pass in model position information. */
+    private int mPositionHandle;
+
+    /** This will be used to pass in model color information. */
+    private int mColorHandle;
+
+    /** How many bytes per float. */
+    private final int mBytesPerFloat = 4;
+
+    /** How many elements per vertex. */
+    private final int mStrideBytes = 7 * mBytesPerFloat;
+
+    /** Offset of the position data. */
+    private final int mPositionOffset = 0;
+
+    /** Size of the position data in elements. */
+    private final int mPositionDataSize = 3;
+
+    /** Offset of the color data. */
+    private final int mColorOffset = 3;
+
+    /** Size of the color data in elements. */
+    private final int mColorDataSize = 4;
+
+    private final Context mContext;
+    private static final String VERTEX_SHADER_FILE = "VertexShader.shd";
+    private static final String FRAGMENT_SHADER_FILE = "FragmentShader.shd";
+
+    /**
+     * Initialize the model data.
+     */
+    public LessonOneRenderer(Context context)
+    {
+        // Define points for equilateral triangles.
+        Log.d(LESSONONE_RENDERER, "LessonOneRenderer");
+
+        mContext = context;
+
+        // This triangle is red, green, and blue.
+        final float[] triangle1VerticesData = {
+                // X, Y, Z,
+                // R, G, B, A
+                -0.5f, -0.25f, 0.0f,
+                1.0f, 0.0f, 0.0f, 1.0f,
+
+                0.5f, -0.25f, 0.0f,
+                0.0f, 0.0f, 1.0f, 1.0f,
+
+                0.0f, 0.559016994f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f};
+
+        // This triangle is yellow, cyan, and magenta.
+        final float[] triangle2VerticesData = {
+                // X, Y, Z,
+                // R, G, B, A
+                -0.5f, -0.25f, 0.0f,
+                1.0f, 1.0f, 0.0f, 1.0f,
+
+                0.5f, -0.25f, 0.0f,
+                0.0f, 1.0f, 1.0f, 1.0f,
+
+                0.0f, 0.559016994f, 0.0f,
+                1.0f, 0.0f, 1.0f, 1.0f};
+
+        // This triangle is white, gray, and black.
+        final float[] triangle3VerticesData = {
+                // X, Y, Z,
+                // R, G, B, A
+                -0.5f, -0.25f, 0.0f,
+                1.0f, 1.0f, 1.0f, 1.0f,
+
+                0.5f, -0.25f, 0.0f,
+                0.5f, 0.5f, 0.5f, 1.0f,
+
+                0.0f, 0.559016994f, 0.0f,
+                0.0f, 0.0f, 0.0f, 1.0f};
+
+        // This triangle is red, green, and blue.
+        final float[] squareVerticesData = {
+                // X, Y, Z,
+                // R, G, B, A
+
+                -0.5f, 0.5f, 0.0f,
+                1.0f, 1.0f, 0.0f, 1.0f,
+
+                -0.5f, -0.5f, 0.0f,
+                0.0f, 1.0f, 1.0f, 1.0f,
+
+                0.5f, 0.5f, 0.0f,
+                1.0f, 0.0f, 1.0f, 1.0f,
+
+                -0.5f, -0.5f, 0.0f,
+                0.0f, 1.0f, 1.0f, 1.0f,
+
+                0.5f, -0.5f, 0.0f,
+                1.0f, 1.0f, 0.0f, 1.0f,
+
+                0.5f, 0.5f, 0.0f,
+                1.0f, 0.0f, 1.0f, 1.0f
+        };
+
+        // Initialize the buffers.
+        mTriangle1Vertices = ByteBuffer
+                .allocateDirect(triangle1VerticesData.length * mBytesPerFloat)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+
+        mTriangle2Vertices = ByteBuffer
+                .allocateDirect(triangle2VerticesData.length * mBytesPerFloat)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+
+        mTriangle3Vertices = ByteBuffer
+                .allocateDirect(triangle3VerticesData.length * mBytesPerFloat)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+
+        mSquareVertices = ByteBuffer
+                .allocateDirect(squareVerticesData.length * mBytesPerFloat)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+
+        mTriangle1Vertices.put(triangle1VerticesData).position(0);
+        mTriangle2Vertices.put(triangle2VerticesData).position(0);
+        mTriangle3Vertices.put(triangle3VerticesData).position(0);
+        mSquareVertices.put(squareVerticesData).position(0);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config)
+    {
+        Log.d(LESSONONE_RENDERER, "OnSurfaceCreated");
+
+        // Set the background clear color to gray.
+        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
+
+        // Eye position
+        final float eyeX = 0.0f;
+        final float eyeY = 0.0f;
+        final float eyeZ = 1.5f;
+
+        // Look At vector
+        final float lookX = 0.0f;
+        final float lookY = 0.0f;
+        final float lookZ = -5.0f;
+
+        // Up vector
+        final float upX = 0.0f;
+        final float upY = 1.0f;
+        final float upZ = 0.0f;
+
+        // Set the view matrix
+        Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
+
+        final String vertexShader = AssetUtils.readShaderFromFile(mContext, VERTEX_SHADER_FILE);
+        final String fragmentShader = AssetUtils.readShaderFromFile(mContext, FRAGMENT_SHADER_FILE);
+
+        if ((vertexShader == null) || (fragmentShader == null)) {
+            throw new RuntimeException(String.format("Failed to load shaders from asset resources files '%s' and '%s'",
+                    VERTEX_SHADER_FILE, FRAGMENT_SHADER_FILE));
+        }
+
+        // Load in the vertex shader.
+        int vertexShaderHandle = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+
+        if (vertexShaderHandle != 0)
+        {
+            // Pass in the shader source.
+            GLES20.glShaderSource(vertexShaderHandle, vertexShader);
+
+            // Compile the shader.
+            GLES20.glCompileShader(vertexShaderHandle);
+
+            // Get the compilation status.
+            final int[] compileStatus = new int[1];
+            GLES20.glGetShaderiv(vertexShaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+            // If the compilation failed, delete the shader.
+            if (compileStatus[0] == 0)
+            {
+                GLES20.glDeleteShader(vertexShaderHandle);
+                vertexShaderHandle = 0;
+            }
+        }
+
+        if (vertexShaderHandle == 0)
+        {
+            throw new RuntimeException("Error creating vertex shader.");
+        }
+
+        // Load in the fragment shader shader.
+        int fragmentShaderHandle = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
+
+        if (fragmentShaderHandle != 0)
+        {
+            // Pass in the shader source.
+            GLES20.glShaderSource(fragmentShaderHandle, fragmentShader);
+
+            // Compile the shader.
+            GLES20.glCompileShader(fragmentShaderHandle);
+
+            // Get the compilation status.
+            final int[] compileStatus = new int[1];
+            GLES20.glGetShaderiv(fragmentShaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+            // If the compilation failed, delete the shader.
+            if (compileStatus[0] == 0)
+            {
+                GLES20.glDeleteShader(fragmentShaderHandle);
+                fragmentShaderHandle = 0;
+            }
+        }
+
+        if (fragmentShaderHandle == 0)
+        {
+            throw new RuntimeException("Error creating fragment shader.");
+        }
+
+        // Create a program object and store the handle to it.
+        int programHandle = GLES20.glCreateProgram();
+
+        if (programHandle != 0)
+        {
+            // Bind the vertex shader to the program.
+            GLES20.glAttachShader(programHandle, vertexShaderHandle);
+
+            // Bind the fragment shader to the program.
+            GLES20.glAttachShader(programHandle, fragmentShaderHandle);
+
+            // Bind attributes
+            GLES20.glBindAttribLocation(programHandle, 0, "a_Position");
+            GLES20.glBindAttribLocation(programHandle, 1, "a_Color");
+
+            // Link the two shaders together into a program.
+            GLES20.glLinkProgram(programHandle);
+
+            // Get the link status.
+            final int[] linkStatus = new int[1];
+            GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, linkStatus, 0);
+
+            // If the link failed, delete the program.
+            if (linkStatus[0] == 0)
+            {
+                GLES20.glDeleteProgram(programHandle);
+                programHandle = 0;
+            }
+        }
+
+        if (programHandle == 0)
+        {
+            throw new RuntimeException("Error creating program.");
+        }
+
+        // Set program handles. These will later be used to pass in values to the program.
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(programHandle, "u_MVPMatrix");
+        mPositionHandle = GLES20.glGetAttribLocation(programHandle, "a_Position");
+        mColorHandle = GLES20.glGetAttribLocation(programHandle, "a_Color");
+
+        // Tell OpenGL to use this program when rendering.
+        GLES20.glUseProgram(programHandle);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height)
+    {
+        Log.d(LESSONONE_RENDERER, "onSurfaceChanged");
+
+        // Set the OpenGL viewport to the same size as the surface.
+        GLES20.glViewport(0, 0, width, height);
+
+        // Create a new perspective projection matrix. The height will stay the same
+        // while the width will vary as per aspect ratio.
+        final float ratio = (float) width / height;
+        final float left = -ratio;
+        final float right = ratio;
+        final float bottom = -1.0f;
+        final float top = 1.0f;
+        final float near = 1.0f;
+        final float far = 10.0f;
+
+        Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 glUnused)
+    {
+
+        GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
+
+        // Do a complete rotation every 10 seconds.
+        long time = SystemClock.uptimeMillis() % 10000L;
+        float angleInDegrees = (360.0f / 10000.0f) * ((int) time);
+
+        Log.d(LESSONONE_RENDERER, "onDrawFrame - " + Long.toString(time));
+
+        // Draw the triangle facing straight on.
+        Matrix.setIdentityM(mModelMatrix, 0);
+        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
+        drawTriangle(mTriangle1Vertices);
+
+        // Draw one translated a bit down and rotated to be flat on the ground.
+        Matrix.setIdentityM(mModelMatrix, 0);
+        Matrix.translateM(mModelMatrix, 0, 0.0f, -1.0f, 0.0f);
+        Matrix.rotateM(mModelMatrix, 0, 90.0f, 1.0f, 0.0f, 0.0f);
+        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
+        drawTriangle(mTriangle2Vertices);
+
+        // Draw one translated a bit to the right and rotated to be facing to the left.
+        Matrix.setIdentityM(mModelMatrix, 0);
+        Matrix.translateM(mModelMatrix, 0, 1.0f, 0.0f, 0.0f);
+        Matrix.rotateM(mModelMatrix, 0, 90.0f, 0.0f, 1.0f, 0.0f);
+        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
+        drawTriangle(mTriangle3Vertices);
+
+        // Draw square facing strait on.
+        float smallerAngle = -angleInDegrees;
+        Matrix.setIdentityM(mModelMatrix, 0);
+        Matrix.translateM(mModelMatrix, 0, 0.0f, 1.0f, 0.0f);
+        //Matrix.rotateM(mModelMatrix, 0, 90.0f, 1.0f, 0.0f, 0.0f);
+        Matrix.rotateM(mModelMatrix, 0, smallerAngle, 0.0f, 0.0f, 1.0f);
+        drawSquare(mSquareVertices);
+    }
+
+    /**
+     * Draws a triangle from the given vertex data.
+     *
+     * @param aTriangleBuffer The buffer containing the vertex data.
+     */
+    private void drawTriangle(final FloatBuffer aTriangleBuffer)
+    {
+        // Pass in the position information
+        aTriangleBuffer.position(mPositionOffset);
+        GLES20.glVertexAttribPointer(mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false,
+                mStrideBytes, aTriangleBuffer);
+
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Pass in the color information
+        aTriangleBuffer.position(mColorOffset);
+        GLES20.glVertexAttribPointer(mColorHandle, mColorDataSize, GLES20.GL_FLOAT, false,
+                mStrideBytes, aTriangleBuffer);
+
+        GLES20.glEnableVertexAttribArray(mColorHandle);
+
+        // This multiplies the view matrix by the model matrix, and stores the result in the MVP matrix
+        // (which currently contains model * view).
+        Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
+
+        // This multiplies the modelview matrix by the projection matrix, and stores the result in the MVP matrix
+        // (which now contains model * view * projection).
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
+
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+    }
+
+    private void drawSquare(final FloatBuffer aSquareBuffer) {
+        // Pass in the position information
+        aSquareBuffer.position(mPositionOffset);
+        GLES20.glVertexAttribPointer(mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false,
+                mStrideBytes, aSquareBuffer);
+
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Pass in the color information
+        aSquareBuffer.position(mColorOffset);
+        GLES20.glVertexAttribPointer(mColorHandle, mColorDataSize, GLES20.GL_FLOAT, false,
+                mStrideBytes, aSquareBuffer);
+
+        GLES20.glEnableVertexAttribArray(mColorHandle);
+
+        // This multiplies the view matrix by the model matrix, and stores the result in the MVP matrix
+        // (which currently contains model * view).
+        Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
+
+        // This multiplies the modelview matrix by the projection matrix, and stores the result in the MVP matrix
+        // (which now contains model * view * projection).
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
+
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
+    }
+}
